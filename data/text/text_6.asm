4a4b58f5690c0d2e57ca921f908604323b2094a5
Mick Kirkegaard <mickkn@gmail.com>
2023-02-06 07:56:30 +0100

    text_6

--- data/text/text_6.asm
+++ data/text/text_6.asm
@@ -1,71 +1,72 @@
 _ItemUseBallText00::
-	text "It dodged the"
-	line "thrown BALL!"
+	text "Den undveg den"
+	line "kastet BALL!"
 
-	para "This #MON"
-	line "can't be caught!"
+	para "Denne #MON"
+	line "kan ikke fanges!"
 	prompt
 
 _ItemUseBallText01::
-	text "You missed the"
+	text "Du ramte ikke den"
 	line "#MON!"
 	prompt
 
 _ItemUseBallText02::
-	text "Darn! The #MON"
-	line "broke free!"
+	text "Øv! #MONen"
+	line "slap fri!"
 	prompt
 
 _ItemUseBallText03::
-	text "Aww! It appeared"
-	line "to be caught! "
+	text "Åh! Den så ud til"
+	line "at være fanget!"
 	prompt
 
 _ItemUseBallText04::
-	text "Shoot! It was so"
-	line "close too!"
+	text "Altså! Det var"
+	line "SÅ tæt på!"
 	prompt
 
 _ItemUseBallText05::
-	text "All right!"
+	text "Godt gået!"
 	line "@"
 	text_ram wEnemyMonNick
-	text " was"
-	cont "caught!@"
+	text " blev"
+	cont "fanget!@"
 	text_end
 
 _ItemUseBallText07::
 	text_ram wBoxMonNicks
-	text " was"
-	line "transferred to"
+	text " blev"
+	line "overført to"
 	cont "BILL's PC!"
 	prompt
 
 _ItemUseBallText08::
 	text_ram wBoxMonNicks
-	text " was"
-	line "transferred to"
-	cont "someone's PC!"
+	text " blev"
+	line "overført til"
+	cont "nogens PC!"
 	prompt
 
 _ItemUseBallText06::
-	text "New #DEX data"
-	line "will be added for"
+	text "Ny #DEX data"
+	line "vil blive"
+	cont "tilføjet for"
 	cont "@"
 	text_ram wEnemyMonNick
 	text "!@"
 	text_end
 
 _SurfingGotOnText::
-	text "<PLAYER> got on"
+	text "<PLAYER> kom op på"
 	line "@"
 	text_ram wcd6d
 	text "!"
 	prompt
 
 _SurfingNoPlaceToGetOffText::
-	text "There's no place"
-	line "to get off!"
+	text "Der er ingen steder"
+	line "at stige af!"
 	prompt
 
 _VitaminStatRoseText::
@@ -73,115 +74,116 @@ _VitaminStatRoseText::
 	text "'s"
 	line "@"
 	text_ram wStringBuffer
-	text " rose."
+	text " steg."
 	prompt
 
 _VitaminNoEffectText::
-	text "It won't have any"
-	line "effect."
+	text "Det vil ikke have"
+	line "nogen effekt."
 	prompt
 
 _ThrewBaitText::
-	text "<PLAYER> threw"
-	line "some BAIT."
+	text "<PLAYER> kastede"
+	line "noget BAIT."
 	done
 
 _ThrewRockText::
-	text "<PLAYER> threw a"
-	line "ROCK."
+	text "<PLAYER> kastede"
+	line "en ROCK."
 	done
 
 _PlayedFluteNoEffectText::
-	text "Played the #"
+	text "Spillede på #"
 	line "FLUTE."
 
-	para "Now, that's a"
-	line "catchy tune!"
+	para "Det er nu en"
+	line "fængende melodi!"
 	prompt
 
 _FluteWokeUpText::
-	text "All sleeping"
-	line "#MON woke up."
+	text "Alle sovende"
+	line "#MON vågnede."
 	prompt
 
 _PlayedFluteHadEffectText::
-	text "<PLAYER> played the"
+	text "<PLAYER> spillede"
 	line "# FLUTE.@"
 	text_end
 
 _CoinCaseNumCoinsText::
-	text "Coins"
+	text "Mønter"
 	line "@"
 	text_bcd wPlayerCoins, 2 | LEADING_ZEROES | LEFT_ALIGN
 	text " "
 	prompt
 
 _ItemfinderFoundItemText::
-	text "Yes! ITEMFINDER"
-	line "indicates there's"
-	cont "an item nearby."
+	text "JA! ITEMFINDER"
+	line "indikere at"
+	cont "en genstand"
+	cont "er tæt på."
 	prompt
 
 _ItemfinderFoundNothingText::
-	text "Nope! ITEMFINDER"
-	line "isn't responding."
+	text "Nææ! ITEMFINDER"
+	line "reagerer ikke."
 	prompt
 
 _RaisePPWhichTechniqueText::
-	text "Raise PP of which"
-	line "technique?"
+	text "Forøg PP for"
+	line "hvilken evne?"
 	done
 
 _RestorePPWhichTechniqueText::
-	text "Restore PP of"
-	line "which technique?"
+	text "Gendan PP for"
+	line "hvilken evne?"
 	done
 
 _PPMaxedOutText::
 	text_ram wStringBuffer
 	text "'s PP"
-	line "is maxed out."
+	line "er maksimeret."
 	prompt
 
 _PPIncreasedText::
 	text_ram wStringBuffer
 	text "'s PP"
-	line "increased."
+	line "forøget."
 	prompt
 
 _PPRestoredText::
-	text "PP was restored."
+	text "PP blev gendannet."
 	prompt
 
 _BootedUpTMText::
-	text "Booted up a TM!"
+	text "Booted en TM op!"
 	prompt
 
 _BootedUpHMText::
-	text "Booted up an HM!"
+	text "Booted en HM op!"
 	prompt
 
 _TeachMachineMoveText::
-	text "It contained"
+	text "Det indeholdt"
 	line "@"
 	text_ram wStringBuffer
 	text "!"
 
-	para "Teach @"
+	para "Lær @"
 	text_ram wStringBuffer
 	text_start
-	line "to a #MON?"
+	line "til en #MON?"
 	done
 
 _MonCannotLearnMachineMoveText::
 	text_ram wcd6d
-	text " is not"
-	line "compatible with"
+	text " er ikke"
+	line "kompatibelt med"
 	cont "@"
 	text_ram wStringBuffer
 	text "."
 
-	para "It can't learn"
+	para "Den kan ikke lære"
 	line "@"
 	text_ram wStringBuffer
 	text "."
@@ -189,43 +191,44 @@ _MonCannotLearnMachineMoveText::
 
 _ItemUseNotTimeText::
 	text "OAK: <PLAYER>!"
-	line "This isn't the"
-	cont "time to use that! "
+	line "Det er ikke tid"
+	cont "til at bruge den!"
 	prompt
 
 _ItemUseNotYoursToUseText::
-	text "This isn't yours"
-	line "to use!"
+	text "Det er ikke din"
+	line "at bruge!"
 	prompt
 
 _ItemUseNoEffectText::
-	text "It won't have any"
-	line "effect."
+	text "Det vil ikke have" 
+	line "nogen effekt."
 	prompt
 
 _ThrowBallAtTrainerMonText1::
-	text "The trainer"
-	line "blocked the BALL!"
+	text "Træneren"
+	line "blokerede BALLen!"
 	prompt
 
 _ThrowBallAtTrainerMonText2::
-	text "Don't be a thief!"
+	text "Vær ikke en tyv!"
 	prompt
 
 _NoCyclingAllowedHereText::
-	text "No cycling"
-	next "allowed here."
+	text "Ingen cykling"
+	next "er tilladt her."
 	prompt
 
 _NoSurfingHereText::
-	text "No SURFing on"
+	text "Ingen SURFing på"
 	line "@"
 	text_ram wcd6d
-	text " here!"
+	text " her!"
 	prompt
 
 _BoxFullCannotThrowBallText::
-	text "The #MON BOX"
-	line "is full! Can't"
-	cont "use that item!"
+	text "#MON BOKSEN"
+	line "er fuld! Kan ikke"
+	cont "bruge den"
+	cont "genstand!"
 	prompt
