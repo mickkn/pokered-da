0c9e2052fa7590dad12c8182c38658adf97559e7
Mick Kirkegaard <mickkn@gmail.com>
2023-02-15 07:37:33 +0100

    BrunosRoom

--- text/BrunosRoom.asm
+++ text/BrunosRoom.asm
@@ -1,37 +1,40 @@
 _BrunoBeforeBattleText::
-	text "I am BRUNO of"
+	text "Jeg er BRUNO fra"
 	line "ELITE-FIREN!"
 
-	para "Through rigorous"
-	line "training, people"
-	cont "and #MON can"
-	cont "become stronger!"
+	para "Gennem hård"
+	line "træning kan" 
+	cont "mennesker og"
+	cont "#MON blive"
+	cont "stærkere!" 
 
-	para "I've weight"
-	line "trained with"
-	cont "my #MON!"
+	para "Jeg har trænet"
+	line "vægte sammen med"
+	cont "mine #MON!"
 
 	para "<PLAYER>!"
 
-	para "We will grind you"
-	line "down with our"
-	cont "superior power!"
+	para "Vi vil save dig"
+	line "ned med vores"
+	cont "overlegne kraft!"
 
 	para "Hoo hah!"
 	done
 
 _BrunoEndBattleText::
-	text "Why?"
-	line "How could I lose?"
+	text "Hvorfor?"
+	line "hvordan kunne"
+	cont "jeg tabe?"
 	prompt
 
 _BrunoAfterBattleText::
-	text "My job is done!"
-	line "Go face your next"
-	cont "challenge!"
+	text "Mit arbejde er"
+	line "færdigt! Gå videre"
+	cont "til din næste"
+	cont "udfordring!"
 	done
 
 _BrunoDontRunAwayText::
-	text "Someone's voice:"
-	line "Don't run away!"
+	text "En stemme siger:"
+	line "Løb ikke væk!"
 	done
