331494025acf01d3965384475a4520a5857f2892
Mick Kirkegaard <mickkn@gmail.com>
2023-03-09 07:47:12 +0100

    CeldaonMark3F

--- text/CeladonMart3F.asm
+++ text/CeladonMart3F.asm
@@ -1,99 +1,105 @@
 _TM18PreReceiveText::
-	text "Oh, hi! I finally"
-	line "finished #MON!"
+	text "Oh hej! Jeg er"
+	line "endelig færdig"
+	cont "med #MON!"
 
-	para "Not done yet?"
-	line "This might be"
-	cont "useful!"
+	para "Ikke færdig endnu?"
+	line "Dette kan være"
+	cont "nyttigt!"
 	prompt
 
 _ReceivedTM18Text::
-	text "<PLAYER> received"
+	text "<PLAYER> modtog"
 	line "@"
 	text_ram wStringBuffer
 	text "!@"
 	text_end
 
 _TM18ExplanationText::
-	text "TM18 is COUNTER!"
-	line "Not like the one"
-	cont "I'm leaning on,"
-	cont "mind you!"
+	text "TM18 er MODANGREB!"
+	line "Ikke den, jeg"
+	cont "støtter mig til,"
+	cont "vel at mærke!"
 	done
 
 _TM18NoRoomText::
-	text "Your pack is full"
-	line "of items!"
+	text "Din taske er fuld"
+	line "af genstande!"
 	done
 
 _CeladonMart3Text2::
-	text "Captured #MON"
-	line "are registered"
-	cont "with an ID No."
-	cont "and OT, the name"
-	cont "of the Original"
-	cont "Trainer that"
-	cont "caught it!"
+	text "Fanget #MON"
+	line "er registreret"
+	cont "med et ID-nummer"
+	cont "og OT, navnet på"
+	cont "den Oprindelige"
+	cont "Træner, der"
+	cont "fangede den!"
 	done
 
 _CeladonMart3Text3::
-	text "All right!"
+	text "Okay!"
 
-	para "My buddy's going"
-	line "to trade me his"
-	cont "KANGASKHAN for my"
-	cont "GRAVELER!"
+	para "Min ven vil bytte"
+	line "sin KANGASKHAN"
+	cont "med min GRAVELER!"
 	done
 
 _CeladonMart3Text4::
-	text "Come on GRAVELER!"
+	text "Kom nu GRAVELER!"
 
-	para "I love GRAVELER!"
-	line "I collect them!"
+	para "Jeg elsker"
+	line "GRAVELER! Jeg"
+	cont "samler på dem!"
 
-	para "Huh?"
+	para "Hvad?"
 
-	para "GRAVELER turned"
-	line "into a different"
+	para "GRAVELER"
+	line "forvandlede sig"
+	cont "til en anden"
 	cont "#MON!"
 	done
 
 _CeladonMart3Text5::
-	text "You can identify"
-	line "#MON you got"
-	cont "in trades by"
-	cont "their ID Numbers!"
+	text "Du kan"
+	line "identificere"
+	cont "#MON, du får"
+	cont "i byttehandler,"
+	cont "ved deres"
+	cont "ID-numre!"
 	done
 
 _CeladonMart3Text6::
-	text "It's an SNES!"
+	text "Det er en SNES!"
 	done
 
 _CeladonMart3Text7::
-	text "An RPG! There's"
-	line "no time for that!"
+	text "Et RPG! Der er"
+	line "ingen tid til det!"
 	done
 
 _CeladonMart3Text9::
-	text "A sports game!"
-	line "Dad'll like that!"
+	text "Et sports spil!"
+	line "Far vil kunne"
+	cont "lide det!"
 	done
 
 _CeladonMart3Text11::
-	text "A puzzle game!"
-	line "Looks addictive!"
+	text "En puzzle!"
+	line "Ser afhængigheds-"
+	cont "skabende ud!"
 	done
 
 _CeladonMart3Text13::
-	text "A fighting game!"
-	line "Looks tough!"
+	text "Et kampspil!"
+	line "Ser svært ud!"
 	done
 
 _CeladonMart3Text14::
-	text "3F: TV GAME SHOP"
+	text "3F: TV SPILBUTIK"
 	done
 
 _CeladonMart3Text15::
-	text "Red and Blue!"
-	line "Both are #MON!"
+	text "Rød og blå!"
+	line "Begge er #MON!"
 	done
