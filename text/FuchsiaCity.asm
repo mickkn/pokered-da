0c95e901a2d269d0a0cbd76da143216ead345a27
Mick Kirkegaard <mickkn@gmail.com>
2024-12-18 07:13:16 +0100

    FuchsiaCity

--- text/FuchsiaCity.asm
+++ text/FuchsiaCity.asm
@@ -1,29 +1,33 @@
 _FuchsiaCityText1::
-	text "Did you try the"
-	line "SAFARI GAME? Some"
-	cont "#MON can only"
-	cont "be caught there."
+	text "Har du prøvet"
+	line "SAFARI SPILLET?"
+	cont "Nogle #MON"
+	cont "kan kun"
+	cont "fanges der."
 	done
 
 _FuchsiaCityText2::
-	text "SAFARIZONEN has a"
-	line "zoo in front of"
-	cont "the entrance."
+	text "SAFARIZONEN har"
+	line "en zoo foran"
+	cont "indgangen."
 
-	para "Out back is the"
-	line "SAFARI GAME for"
-	cont "catching #MON."
+	para "Bagved er SAFARI"
+	line "SPILLET, hvor"
+	cont "du kan fange"
+	cont "#MON."
 	done
 
 _FuchsiaCityText3::
-	text "ERIK: Where's"
-	line "SARA? I said I'd"
-	cont "meet her here."
+	text "ERIK: Hvor er"
+	line "SARA? Jeg sagde,"
+	cont "at jeg ville"
+	cont "møde hende her."
 	done
 
 _FuchsiaCityText4::
-	text "That item ball in"
-	line "there is really a"
+	text "Den kugle"
+	line "derinde er"
+	cont "faktisk en"
 	cont "#MON."
 	done
 
@@ -33,13 +37,13 @@ _FuchsiaCityText5::
 
 _FuchsiaCityText11::
 	text "FUCHSIA CITY"
-	line "Behold! It's"
+	line "Se her! Det er"
 	cont "Passion Pink!"
 	done
 
 _FuchsiaCityText13::
-	text "SAFARI GAME"
-	line "#MON-U-CATCH!"
+	text "SAFARI SPILLET"
+	line "#MON-DU-FANGER!"
 	done
 
 _FuchsiaCityText16::
@@ -56,63 +60,64 @@ _FuchsiaCityText17::
 _FuchsiaCityText18::
 	text "FUCHSIA CITY"
 	line "#MON GYM"
-	cont "LEADER: KOGA"
+	cont "LEDER: KOGA"
 
-	para "The Poisonous"
-	line "Ninja Master"
+	para "Den giftige"
+	line "Ninja Mester"
 	done
 
 _FuchsiaCityChanseyText::
 	text "Name: CHANSEY"
 
-	para "Catching one is"
-	line "all up to chance."
+	para "At fange en"
+	line "afhænger helt"
+	cont ""
 	prompt
 
 _FuchsiaCityVoltorbText::
 	text "Name: VOLTORB"
 
-	para "The very image of"
-	line "a # BALL."
+	para "Selve billedet på"
+	line "en # BALL."
 	prompt
 
 _FuchsiaCityKangaskhanText::
 	text "Name: KANGASKHAN"
 
-	para "A maternal #MON"
-	line "that raises its"
-	cont "young in a pouch"
-	cont "on its belly."
+	para "En moderlig #MON,"
+	line "der opfostrer"
+	cont "sine unger i en"
+	cont "pung på maven."
 	prompt
 
 _FuchsiaCitySlowpokeText::
 	text "Name: SLOWPOKE"
 
-	para "Friendly and very"
-	line "slow moving."
+	para "Venlig og meget"
+	line "langsommelig."
 	prompt
 
 _FuchsiaCityLaprasText::
 	text "Name: LAPRAS"
 
-	para "A.K.A. the king"
-	line "of the seas."
+	para "A.K.A. havets "
+	line "konge."
 	prompt
 
 _FuchsiaCityOmanyteText::
 	text "Name: OMANYTE"
 
-	para "A #MON that"
-	line "was resurrected"
-	cont "from a fossil."
+	para "En #MON der"
+	line "blev genoplivet"
+	cont "fra et fossil."
 	prompt
 
 _FuchsiaCityKabutoText::
 	text "Name: KABUTO"
 
-	para "A #MON that"
-	line "was resurrected"
-	cont "from a fossil."
+	para "En #MON der blev"
+	line "genoplivet"
+	cont "fra et fossil."
 	prompt
 
 _FuchsiaCityText_19b2a::
